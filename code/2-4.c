ee86807f9cf9d93f89ac141ec6c24b5457199c82
hyojeong-commits <kweunsungo@gmail.com>
2026-04-09 21:40:37 +0900

    Update 2-4.c

---
+++ code/2-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+long my_strol(char *str, char **endptr){
+    long res = 0;
+
+    while()
+}
+
+int main(){
+    char str[] = "2026hello";
+    char *end;
+
+    long val = my_strol(str, &end);
+
+    printf("%1d %s\n", val, end);
+    return 0;
+}
