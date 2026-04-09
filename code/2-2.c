f4c9cd14d845baa0da9a97e2bb2b381ab99760c0
hyojeong-commits <kweunsungo@gmail.com>
2026-04-09 23:43:27 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -2,13 +2,13 @@
 #include <string.h>
 
 void sort_strings(char **arr, int size){
-    char *alp;
+    char *temp;
     for(int i=0; i < size - 1; i++){
         for(int j = 0; j < size -1 - i; j++){
-            if (strcmp(arr[j], arr[j+1])>0){
-                alp = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=alp;
+          if (strcmp(*(arr + j), *(arr + (j + 1))) > 0) {
+                temp = *(arr + j);
+                *(arr + j) = *(arr + (j + 1));
+                *(arr + (j + 1)) = temp;
             }
         }
     }
